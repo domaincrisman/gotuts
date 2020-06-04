9773e7a9684f97727a4284a0a5789dbf5f20d244
domaincrisman <cmanzanore@gmail.com>
2020-06-04 17:12:31 +0200

    add_users_to_courses relationship

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
+#User.create!(email: 'admin@example.com', password: 'Cadiar2020', password_confirmation: 'Cadiar2020')
+
 30.times do
   Course.create!([{
     title: Faker::Educator.course_name,
-    description: Faker::TvShows::GameOfThrones.quote
+    description: Faker::TvShows::GameOfThrones.quote,
+    user_id: User.first.id
   }])
 end
