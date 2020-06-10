5c6acd6b26a087c5a96dd4020e326640f2376757
domaincrisman <cmanzanore@gmail.com>
2020-06-10 13:15:55 +0200

    gem pundit - only admin can edit other users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,11 @@ class UsersController < ApplicationController
   end
 
   def edit
+    authorize @user
   end
-
+  
   def update
+    authorize @user
     if @user.update(user_parms)
       redirect_to users_path, notice: 'User roles were successfully updated'
     else
