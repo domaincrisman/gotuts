28e858675f793bf1a27c066a1061f2d0691db3f5
domaincrisman <cmanzanore@gmail.com>
2020-06-10 12:49:16 +0200

    gem pundit - authorization for courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -24,17 +24,19 @@ class CoursesController < ApplicationController
   # GET /courses/new
   def new
     @course = Course.new
+    authorize @course
   end
 
   # GET /courses/1/edit
   def edit
     authorize @course
   end
-
+  
   # POST /courses
   # POST /courses.json
   def create
     @course = Course.new(course_params)
+    authorize @course
     @course.user = current_user
     
     respond_to do |format|
@@ -47,10 +49,11 @@ class CoursesController < ApplicationController
       end
     end
   end
-
+  
   # PATCH/PUT /courses/1
   # PATCH/PUT /courses/1.json
   def update
+    authorize @course
     respond_to do |format|
       if @course.update(course_params)
         format.html { redirect_to @course, notice: 'Course was successfully updated.' }
@@ -61,10 +64,11 @@ class CoursesController < ApplicationController
       end
     end
   end
-
+  
   # DELETE /courses/1
   # DELETE /courses/1.json
   def destroy
+    authorize @course
     @course.destroy
     respond_to do |format|
       format.html { redirect_to courses_url, notice: 'Course was successfully destroyed.' }
