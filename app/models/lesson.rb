e206dc0abe87db6d5325cdf2b4a0854b3d09967f
domaincrisman <cmanzanore@gmail.com>
2020-06-11 19:47:50 +0200

    Add action text for lessons

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,6 +2,8 @@ class Lesson < ApplicationRecord
   belongs_to :course
   validates :title, :content, :course, presence: true
 
+  has_rich_text :content
+
   extend FriendlyId
   friendly_id :title, use: :slugged
 
@@ -11,4 +13,5 @@ class Lesson < ApplicationRecord
   def to_s
     title
   end
+
 end
