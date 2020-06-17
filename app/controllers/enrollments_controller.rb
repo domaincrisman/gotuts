90f53e7cd64c05e82107911399aaaa30ea0ff9d5
domaincrisman <cmanzanore@gmail.com>
2020-06-17 13:55:32 +0200

    gem ransack for enrollments index

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -3,8 +3,12 @@ class EnrollmentsController < ApplicationController
   before_action :set_course, only: [:new, :create]
 
   def index
-    @enrollments = Enrollment.all
-    @pagy, @enrollments = pagy(Enrollment.all)
+    # @enrollments = Enrollment.all
+    #@pagy, @enrollments = pagy(Enrollment.all)
+
+    @q = Enrollment.ransack(params[:q])
+    @pagy, @enrollments = pagy(@q.result.includes(:user))
+
     authorize @enrollments
   end
 
