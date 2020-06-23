8f2b3389893086b7a1946b8915c508fd93a4447b
domaincrisman <cmanzanore@gmail.com>
2020-06-23 21:42:32 +0200

    namespace charts in routes, group by sum

--- config/routes.rb
+++ config/routes.rb
@@ -5,19 +5,28 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'activity', to: 'home#activity'
   get 'analytics', to: 'home#analytics'
-  get 'charts/users_per_day', to: 'charts#users_per_day'
-  get 'charts/enrollments_per_day', to: 'charts#enrollments_per_day'
-  get 'charts/course_popularity', to: 'charts#course_popularity'
-
+  
   resources :enrollments do
     get :my_students, on: :collection
   end
+
   resources :courses do
     get :purchased, :pending_review, :created, on: :collection
     resources :lessons
-    resources :enrollments, only: [:new, :create]
-
+    resources :enrollments, only: [:new, :create]   
   end
+
   resources :users, only: [:index, :edit, :show, :update]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  namespace :charts do
+    get 'users_per_day'
+    get 'enrollments_per_day'
+    get 'course_popularity'
+    get 'money_makers'
+  end
+# get 'charts/users_per_day', to: 'charts#users_per_day'
+# get 'charts/enrollments_per_day', to: 'charts#enrollments_per_day'
+# get 'charts/course_popularity', to: 'charts#course_popularity'
+# get 'charts/money_makers', to: 'charts#money_makers'
+  
 end
