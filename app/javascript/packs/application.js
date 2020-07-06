f04fbf4d01360f2681aa58ee5cc69d2877fe0bc1
domaincrisman <cmanzanore@gmail.com>
2020-07-06 20:01:06 +0200

    action_text attachment restrictions, download and display

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,6 +23,8 @@ require("@rails/actiontext")
 require("chartkick") // yarn add chartkick chart.js
 require("chart.js")
 
+import "../trix-editor-overrides" 
+
 require("jquery") // yarn add jquery
 require("jquery-ui-dist/jquery-ui"); // yarn add jquery-ui-dist 
 
