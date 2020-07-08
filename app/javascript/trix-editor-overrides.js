6c41b343e59bc7b1425ae0813339690768052359
domaincrisman <cmanzanore@gmail.com>
2020-07-08 16:25:56 +0200

    action_text - allow file upload, add video preview to blob

--- app/javascript/trix-editor-overrides.js
+++ app/javascript/trix-editor-overrides.js
@@ -1,8 +1,8 @@
 // Blacklist all file attachments
-window.addEventListener("trix-file-accept", function(event) {
-  event.preventDefault()
-  alert("File attachment not supported!")
-})
+// window.addEventListener("trix-file-accept", function(event) {
+//   event.preventDefault()
+//   alert("File attachment not supported!")
+// })
 
 
 // Only images
@@ -16,10 +16,10 @@ window.addEventListener("trix-file-accept", function(event) {
 //})
 
 // File size
-// window.addEventListener("trix-file-accept", function(event) {
-//   const maxFileSize = 1024 * 1024 // 1MB 
-//   if (event.file.size > maxFileSize) {
-//     event.preventDefault()
-//     alert("Only support attachment files upto size 1MB!")
-//   }
-// })
+window.addEventListener("trix-file-accept", function(event) {
+  const maxFileSize = 3000 * 3000 // 1MB 
+  if (event.file.size > maxFileSize) {
+    event.preventDefault()
+    alert("Only support attachment files upto size 9MB!")
+  }
+})
