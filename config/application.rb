553a24d73b6d32dc00670b2483314c64c2004e5e
domaincrisman <cmanzanore@gmail.com>
2020-07-08 19:34:03 +0200

    action_text - play audio with HTML5

--- config/application.rb
+++ config/application.rb
@@ -23,9 +23,10 @@ module Rubygems
       ActionText::ContentHelper.allowed_attributes.add 'controls'
 
       ActionText::ContentHelper.allowed_tags.add 'video'
+      ActionText::ContentHelper.allowed_tags.add 'audio'
       ActionText::ContentHelper.allowed_tags.add 'source'
     end
-    
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
