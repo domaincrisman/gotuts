74ad204752dfb78d2eee403f591309e53a8211d2
domaincrisman <cmanzanore@gmail.com>
2020-07-13 19:16:11 +0200

    add counter_cache for user_lessons to lessons and users

--- app/models/user_lesson.rb
+++ app/models/user_lesson.rb
@@ -1,6 +1,7 @@
 class UserLesson < ApplicationRecord
-  belongs_to :user
-  belongs_to :lesson
+
+  belongs_to :user, counter_cache: true
+  belongs_to :lesson, counter_cache: true
 
   validates :user, :lesson, presence: true
   validates_uniqueness_of :user_id, scope: :lesson_id #user cant see the same lesson twice for the first time
