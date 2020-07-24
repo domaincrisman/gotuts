111ae560243faf9e378e7d0e2474e1a8d24ce9e9
domaincrisman <cmanzanore@gmail.com>
2020-07-24 13:59:45 +0200

    two ways to search courses by tags with ransack

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,13 @@
 class Tag < ApplicationRecord
   has_many :course_tags
   has_many :courses, through: :course_tags
+  validates :name, length: {minimum: 1, maximum: 25}, uniqueness: true
+  
+  def to_s
+		name
+	end
 
-	validates :name, length: {minimum: 1, maximum: 25}, uniqueness: true
+	def popular_name
+	  "#{name.to_s}: #{course_tags_count.to_s}"
+	end
 end
