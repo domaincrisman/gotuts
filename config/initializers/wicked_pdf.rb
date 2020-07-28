5f9f75d6416210d9fd4c1d942b20018318fbde2e
domaincrisman <cmanzanore@gmail.com>
2020-07-28 20:57:01 +0200

    pdf.html - remove unused code, enrollments/index - link to certificate

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -18,7 +18,11 @@ WickedPdf.config = {
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
   # layout: 'pdf.html',
-
+  layout: "pdf.html.haml",
+  orientation: "Landscape",
+  lowquality: true,
+  zoom: 1,
+  dpi: 75
   # Using wkhtmltopdf without an X server can be achieved by enabling the
   # 'use_xvfb' flag. This will wrap all wkhtmltopdf commands around the
   # 'xvfb-run' command, in order to simulate an X server.
