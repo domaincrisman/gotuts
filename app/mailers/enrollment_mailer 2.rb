902a57032542cffbfd780a905f0980606bd282df
domaincrisman <cmanzanore@gmail.com>
2020-08-11 20:47:25 +0200

    fix user_mailer - send all admins emails when a new user registers

--- app/mailers/enrollment_mailer 2.rb
+++
@@ -1,9 +0,0 @@
-class EnrollmentMailer < ApplicationMailer
-
-  def new_enrollment(enrollment)
-    @enrollment = enrollment
-    @course = @enrollment.course
-    mail(to: @enrollment.user.email, subject: "You have enrolled to: #{@course}" )
-    #mail(to: @enrollment.course.user.email, subject: "You have a new student in: #{@course}" )
-  end
-end
--- test/mailers/previews/enrollment_mailer_preview 2.rb
+++
@@ -1,8 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/enrollment_mailer
-class EnrollmentMailerPreview < ActionMailer::Preview
-
-  def new_enrollment
-    EnrollmentMailer.new_enrollment(Enrollment.last).deliver_now
-  end
-
-end
