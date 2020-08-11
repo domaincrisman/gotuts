22803188e09886ffbbc00749be839070a0831223
domaincrisman <cmanzanore@gmail.com>
2020-08-11 20:12:12 +0200

    user_mailer - send all admins emails

---
+++ test/mailers/previews/enrollment_mailer_preview 2.rb
@@ -0,0 +1,8 @@
+# Preview all emails at http://localhost:3000/rails/mailers/enrollment_mailer
+class EnrollmentMailerPreview < ActionMailer::Preview
+
+  def new_enrollment
+    EnrollmentMailer.new_enrollment(Enrollment.last).deliver_now
+  end
+
+end
