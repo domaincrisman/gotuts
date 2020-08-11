97af2bbbc26d2c911fda73e7a2202a9c41a00ec3
domaincrisman <cmanzanore@gmail.com>
2020-08-11 11:59:34 +0200

    ActionMailer - send enrollment confirmation to user

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'support@gotuts-crisman.herokuapp.com'
+  default from: "GoTuts <support@gotuts-crisman.herokuapp.com>"
   layout 'mailer'
 end
