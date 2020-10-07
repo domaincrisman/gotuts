50eb7dc469c2e75883004c0e87199f1e8c4d9001
domaincrisman <cmanzanore@gmail.com>
2020-10-07 15:37:37 +0200

    user has_many :students, through: :courses, source: :enrollments

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
   has_many :courses, dependent: :nullify
   has_many :enrollments, dependent: :nullify
   has_many :user_lessons, dependent: :nullify
+  has_many :students, through: :courses, source: :enrollments
   has_many :comments, dependent: :nullify
   
   include PublicActivity::Model
