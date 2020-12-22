54b2473400afa0a12d076ac23638588efc381bf8
domaincrisman <cmanzanore@gmail.com>
2020-12-22 15:31:45 +0100

    update chapters counter_cache, friendly_id, belong to courses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_22_135912) do
+ActiveRecord::Schema.define(version: 2020_12_22_141559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,7 +71,9 @@ ActiveRecord::Schema.define(version: 2020_12_22_135912) do
     t.bigint "course_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
     t.index ["course_id"], name: "index_chapters_on_course_id"
+    t.index ["slug"], name: "index_chapters_on_slug", unique: true
   end
 
   create_table "comments", force: :cascade do |t|
@@ -108,6 +110,7 @@ ActiveRecord::Schema.define(version: 2020_12_22_135912) do
     t.boolean "published", default: false
     t.boolean "approved", default: false
     t.integer "income", default: 0, null: false
+    t.integer "chapters_count", default: 0, null: false
     t.index ["slug"], name: "index_courses_on_slug", unique: true
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
